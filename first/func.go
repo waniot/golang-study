a2277735a43272b61a1da948a6d6134e6da7319c
Tinywan <756684177@qq.com>
2022-01-10 22:17:18 +0800

    Hello World

---
+++ first/func.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+// (1)返回值和类型
+func fun1(a string, b int) int {
+	fmt.Println("a ", a)
+	fmt.Println("b ", b)
+
+	c := 100
+	return c
+}
+
+// (2) 多个返回值和类型
+func fun2(a string, b int) (int, string) {
+	fmt.Println("a ", a)
+	fmt.Println("b ", b)
+
+	c := 100
+	d := "Hi Tinywan"
+	return c, d
+}
+
+// (3) 多个返回值和类型，指定返回值名称
+func fun3(a string, b int) (r1 int, r2 string) {
+	fmt.Println("a ", a)
+	fmt.Println("b ", b)
+
+	r1 = 2022
+	r2 = "Tinywan"
+	return
+}
+
+func main() {
+	fun1("Tinywan", 2022)
+
+	res1, res2 := fun2("PHP", 20222)
+	fmt.Println("res1 = ", res1, "res2 = ", res2)
+
+	r1, r2 := fun3("Golang", 1990)
+	fmt.Println("r1 = ", r1, "r2 = ", r2)
+}
--- first/main.go
+++ first/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+var gA int = 100
+
+// gB := 200 这样子不支持全局变量
+
+// 多变量声明
+var gC, gD = 200, 300
+var (
+	gE int  = 400
+	gF bool = false
+)
+
+// 定义常量
+const BEIJING = 10086
+const SHANGHAI = 10000
 
 func main() {
+	time.Sleep(1 * time.Second)
 	fmt.Println("Hello World")
+
+	a := 1
+	fmt.Printf("a type is = %T\n", a)
+
+	fmt.Println("gA = ", gA)
+	fmt.Println("gC = ", gC)
+	fmt.Println("gD = ", gD)
+	fmt.Println("gE = ", gE)
+	fmt.Println("gF = ", gF)
 }
